ec7547e7fd1b3126992c5965e0b85978e1514de3
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-07-06 13:11:05 +0300

    Add files via upload

---
+++ Java Multithreading 2303 Disallow class instances/src/com/company/Solution.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Solution {
+    public abstract static class Listener {
+
+    public final void onMouseDown(int x, int y) {
+
+    }
+    public void onMouseUp(int x, int y) {
+
+    }
+    }
+    public static void main(String[] args) {
+
+    }
+}
